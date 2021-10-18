80eebd0e9a5625324a2e6bb6b33d6a9911bb54cc
May <MayyFaucher@gmail.com>
2021-10-17 19:59:20 -0700

    got HTML ready for content, all that's left to do is figure out how to get the 'getRole' function to work without re-typing it 3-4 times.

--- index.js
+++ index.js
@@ -1,65 +1,52 @@
 const Inquirer = require("inquirer")
 
 const Manager = require("./lib/Manager")
+// const addManagers = require("./lib/Manager")
 const Intern = require("./lib/Intern")
 const Engineer = require("./lib/Engineer")
+const Employee = require("./lib/Employee")
 
-
-
-
-
-
-
+// Default unchanged question list.
+// Only has list selection.
 const bigAsk = [{
     type: "list",
     message: "Would you like to add a new Employee to the roster? \n --",
-    choices: ["Add a new Manager.","Add a new Engineer.","Add a new Intern.","That's all, thankyou."],
+    choices: ["Add a new Manager.", "Add a new Engineer.", "Add a new Intern.", "That is all, thankyou."],
     name: "classSelection"
 }]
-
-
-
-
-
-
-
-
-function start() {
-
-    Inquirer
-        .prompt(bigAsk)
-        .then( response => choices(response))
-}
-
-// Alternatively this could be inside a singular add person function
-// which removes the repitition involved in re-creating the bigAsk/questions array.
-// That would require me to refractor two main functions into one however, so I can save it for later.
-function choices(response){
-    if(response.classSelection == 'Add a new Manager.') {
-        addManagers()
-    } else if (response.classSelection == 'Add a new Engineer.') {
-        addEngineers()
-    } else if (response.classSelection == 'Add a new Intern.') {
-        addInterns()
-    } else if (response.classSelection == "That's all, thankyou.") {
-        appendPeoples()
-    } else console.log("Oops, that result wasn't expected. Please try again.")
-}
-
-
-
-
-
 
 // This is the array of employees
 // I push them all to this, then I loop through to append it all to the page.
 // super simple.
 const employees = []
 
+// Initialization function
+function start() {
+
+    Inquirer
+        .prompt(bigAsk)
+        .then(response => choices(response))
+}
+
+// Alternatively this could be inside a singular add person function
+// which removes the repitition involved in re-creating the bigAsk/questions array.
+// That would require me to refractor two main functions into one however, so I can save it for later.
+function choices(response) {
+    if (response.classSelection == 'Add a new Manager.') {
+        addManagers()
+    } else if (response.classSelection == 'Add a new Engineer.') {
+        addEngineers()
+    } else if (response.classSelection == 'Add a new Intern.') {
+        addInterns()
+    } else if (response.classSelection == 'That is all, thankyou.') {
+        runItBack();
+    } else console.log("Oops, that result wasn't expected. Please try again.")
+}
 
 
 // I can do this by passing it all through one function, would require refactor
 function addManagers() {
+    // let bigAsk overwrites the constantly set question list with new ones while this function is running.
     let bigAsk = [{
         type: "input",
         message: "What is the name of this Manager? \n --",
@@ -81,7 +68,7 @@ function addManagers() {
         name: "officeNumber"
     }
     ]
-
+    // a new inquiry with the new questions, which creates a new manager.
     Inquirer
         .prompt(bigAsk)
         .then((response) => {
@@ -91,6 +78,7 @@ function addManagers() {
             start()
         })
 }
+
 
 // refractor this in the future.
 function addEngineers() {
@@ -120,11 +108,12 @@ function addEngineers() {
         .prompt(bigAsk)
         .then((response) => {
             // console.log(response);
-            increaseManagers(response.name, response.id, response.email, response.github)
+            increaseEngineers(response.name, response.id, response.email, response.github)
             console.log("Currently, these are the employees", employees)
             start()
         })
 }
+
 
 // refractor this in the future.
 function addInterns() {
@@ -165,7 +154,7 @@ function addInterns() {
 
 /////////////////////////////////////////////////////
 // Used for testing
-// increaseManagers("--Todd", 204, "triangle", "numbies")
+increaseManagers("--Todd", 204, "triangle", "numbies")
 // increaseManagers("Rabecca",264,"square","numbors")
 
 /////////////////////////////////////////////////////
@@ -179,7 +168,7 @@ function increaseManagers(name, id, email, officeNumber) {
 
 ///////////////////////////////////////////////////
 // Used for testing
-increaseEngineers("--acacia", 824, "filiment", "tundra")
+// increaseEngineers("--acacia", 824, "filiment", "tundra")
 // increaseEngineers("Oak",254,"fiber","jungle")
 
 ///////////////////////////////////////////////////
@@ -201,19 +190,44 @@ function increaseInterns(name, id, email, school) {
     employees.push(name)
 }
 
-// function loopThePeople() {
-//     for (var people of employees) {
+
+function runItBack() {
+    for (i=0; i<employees.length;i++) {
+        console.log(employees[i])
+        // if(employees[i] == Manager) {
+
+        // How DO I DO this part.
+            Employee.getRole(employees[i])
+                getRole(employees[i])
+                console.log(getRole(), "this is 1st")
+                console.log(getRole(employees[i]), "this is 2nd")
+        // }
+
+// This will be appended to the page.
+
+        // <div class="card" style="width: 18rem;">
+        //     <div class ="card-body">
+        //         <h5 class ="card-title">Manager</h5>
+        //         <p class ="card-text">e bulk of the card's content.</p>
+        //     </div>
+        //     <ul class ="list-group list-group-flush">
+        //         <li class ="list-group-item">An item</li>
+        //         <li class ="list-group-item">A second item</li>
+        //         <li class ="list-group-item">A third item</li>
+        //     </ul>
+        //     <div class ="card-body">
+        //         <a href="#" class ="card-link">Card link</a>
+        //         <a href="#" class ="card-link">Another link</a>
+        //     </div>
+        // </div>
 
 
-
-
-
-//     }
-// }
+    }
+}
 
 console.log(employees)
 
 
 start()
 
-// module.exports =
+module.exports = start;
